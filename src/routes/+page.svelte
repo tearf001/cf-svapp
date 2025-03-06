1b5b7676f7a4a243fa31bebe2126e98d22486749
tearf001 <18907191365@189.cn>
2025-03-06 15:51:07 +0800

    add nprogress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    // @ts-ignore    
+    import NProgress from 'nprogress';
+    import {beforeNavigate, afterNavigate} from '$app/navigation'
+
+    NProgress.configure({ showSpinner: false });
+    beforeNavigate(({}) => {
+        NProgress.start(); 
+    });
+    afterNavigate(() => {
+        NProgress.done();
+    });
+
+</script>
+<h1 class="text-3xl">Welcome to SvelteKit</h1>
+<p class="text-2xl text-gray-500 container">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
