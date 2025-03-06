523c34e9bc3e68b17f813c89421491b3f3c733ab
tearf001 <18907191365@189.cn>
2025-03-06 15:57:31 +0800

    before twup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
+	// @ts-ignore
+	import NProgress from 'nprogress';
+	import { beforeNavigate, afterNavigate } from '$app/navigation';
 	import '../app.css';
+
+	NProgress.configure({ showSpinner: false });
+	beforeNavigate(({}) => {
+		NProgress.start();
+	});
+	afterNavigate(() => {
+		NProgress.done();
+	});
+
 	let { children } = $props();
 </script>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,3 @@
-<script lang="ts">
-    // @ts-ignore    
-    import NProgress from 'nprogress';
-    import {beforeNavigate, afterNavigate} from '$app/navigation'
 
-    NProgress.configure({ showSpinner: false });
-    beforeNavigate(({}) => {
-        NProgress.start(); 
-    });
-    afterNavigate(() => {
-        NProgress.done();
-    });
-
-</script>
 <h1 class="text-3xl">Welcome to SvelteKit</h1>
 <p class="text-2xl text-gray-500 container">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
