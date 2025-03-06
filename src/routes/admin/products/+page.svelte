0db41812d56fa0c0c7c9323b4955f1f2c42a7080
tearf001 <tearf001@gmail.com>
2025-03-07 02:47:57 +0800

    准备加入表单 chore(vscode): 调整 VSCode 设置和更新包
    - 在 VSCode 设置中添加树形结构缩进为 24 像素
    - 更新 package.json 中的依赖包顺序

---
+++ src/routes/admin/products/+page.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { Input } from '$lib/components/ui/input';
+	import { Button } from '$lib/components/ui/button';
+	import * as Table from '$lib/components/ui/table';
+
+	let { data }: { data: PageData } = $props();
+	let showProducts = $state(true);
+</script>
+
+<div class="space-y-4 p-8 pt-6 flex flex-col">
+	<div class="flex items-center justify-between space-y-2">
+		<h2 class="text-3xl font-bold tracking-tight">产品列表</h2>
+		<div class="flex items-center space-x-2">
+			<Input class="w-40 lg:w-64" type="text" placeholder="搜索产品" />
+			<Button>搜索</Button>
+		</div>
+	</div>
+
+	{#if !showProducts}
+		<Table.Root>
+			<Table.Header>
+				<Table.Row>
+					<Table.Head class="w-[100px]">ID</Table.Head>
+					<Table.Head>图像</Table.Head>
+					<Table.Head>名称</Table.Head>
+					<Table.Head>目录</Table.Head>
+					<Table.Head>子目录</Table.Head>
+					<Table.Head>价格</Table.Head>
+					<Table.Head>库存</Table.Head>
+					<Table.Head class="text-right">操作</Table.Head>
+				</Table.Row>
+			</Table.Header>
+			<Table.Body>
+				<Table.Row>
+					<Table.Cell class="font-medium">1</Table.Cell>
+					<Table.Cell
+						><img
+							src="https://images.stockx.com/360/Air-Jordan-1-Retro-High-OG-Black-Toe-Reimagined-PS/Images/Air-Jordan-1-Retro-High-OG-Black-Toe-Reimagined-PS/Lv2/img21.jpg?w=576&q=57&dpr=2&updated_at=1740500445&h=384"
+							alt=""
+							class="size-14 rounded border"
+						/></Table.Cell
+					>
+					<Table.Cell>乔丹Air 1</Table.Cell>
+					<Table.Cell>鞋类</Table.Cell>
+					<Table.Cell>乔丹品牌</Table.Cell>
+					<Table.Cell>$0</Table.Cell>
+					<Table.Cell>100</Table.Cell>
+					<Table.Cell class="text-right">
+						<Button variant="destructive">删除</Button>
+					</Table.Cell>
+				</Table.Row>
+			</Table.Body>
+		</Table.Root>
+		<span class="text-sm mx-auto text-center">共 {data?.products?.length} 条数据</span>
+	{:else}
+		<p
+			class="flex h-[calc(70vh-200px)] w-full flex-col items-center justify-center gap-2 text-center text-sm text-muted-foreground"
+		>
+			暂无数据
+			<Button href="products/add">添加产品</Button>
+		</p>
+	{/if}
+</div>
---
+++ src/routes/admin/products/add/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import * as Form from '$lib/components/ui/form';
+	import * as Card from '$lib/components/ui/card';
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
+
+	let { data }: { data: PageData } = $props();
+</script>
+
+<div class="space-y-4 p-8">
+	<div class="flex flex-col items-center justify-between space-y-2">
+		<h2 class="text-3xl font-bold tracking-tight">创建产品</h2>
+		<div class="flex items-center space-x-2">
+			<a href="/products/add" class="btn">创建</a>
+		</div>
+		<form action="" enctype="multipart/form-data" method="POST">
+			<Card.Root class="container">
+				<Card.Header>
+					<Card.Title>产品信息</Card.Title>
+					<Card.Description>请填写产品信息</Card.Description>
+				</Card.Header>
+				<Card.Content>
+					<Form.Field {form} name="email">
+						<Form.Control>
+							{#snippet children({ props })}
+								<Form.Label>Email</Form.Label>
+								<Input {...props} bind:value={formData.email} />
+							{/snippet}
+						</Form.Control>
+						<Form.Description />
+						<Form.FieldErrors />
+					</Form.Field>
+				</Card.Content>
+				<Card.Footer>
+					<Button>提交</Button>
+				</Card.Footer>
+			</Card.Root>
+		</form>
+	</div>
+</div>
