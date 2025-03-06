fcc74281eba311847a08affb7ec8f88aa32375aa
tearf001 <18907191365@189.cn>
2025-03-06 16:53:58 +0800

    build(dependencies): 更新项目依赖并添加新库
    - 添加 bits-ui 和 lucide-svelte 库
    - 更新 pnpm-lock.yaml 文件中的依赖版本
    - 在 vite.config.ts 中添加服务器配置
    - ui 是shadcn-svelte, 第一次添加组件 sider bar. 附带安装的一些内容
    - admin的前端layout 注入sidebar 的 内容

---
+++ src/routes/admin/+layout.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import { Blocks, Box, Home, Truck, User, LayoutDashboard } from 'lucide-svelte';
+
+	// Menu items.
+	const items = [
+		{
+			title: '概览',
+			url: 'overview',
+			icon: LayoutDashboard
+		},
+		{
+			title: '产品',
+			url: 'products',
+			icon: Box
+		},
+		{
+			title: '用户',
+			url: 'users',
+			icon: User
+		},
+		{
+			title: '订单',
+			url: 'order',
+			icon: Truck
+		},
+		{
+			title: '分类',
+			url: 'categories',
+			icon: Blocks
+		},
+		{
+			title: '工作台',
+			url: '../',
+			icon: Home
+		}
+	];
+
+	let { children } = $props();
+</script>
+
+<Sidebar.Provider>
+	<Sidebar.Root>
+		<Sidebar.Content>
+			<Sidebar.Group>
+				<Sidebar.GroupLabel>应用程序</Sidebar.GroupLabel>
+				<Sidebar.GroupContent>
+					<Sidebar.Menu>
+						{#each items as item (item.title)}
+							<Sidebar.MenuItem>
+								<Sidebar.MenuButton>
+									{#snippet child({ props })}
+										<a href={'/admin/' + item.url} {...props}>
+											<item.icon />
+											<span>{item.title}</span>
+										</a>
+									{/snippet}
+								</Sidebar.MenuButton>
+							</Sidebar.MenuItem>
+						{/each}
+					</Sidebar.Menu>
+				</Sidebar.GroupContent>
+			</Sidebar.Group>
+		</Sidebar.Content>
+	</Sidebar.Root>
+	<main class=" flex-1">
+		<Sidebar.Trigger />
+		{@render children?.()}
+	</main>
+</Sidebar.Provider>
